edd9c2873923f71653b301780e632b83bcead1bb
David <dmonzonreyes@gmail.com>
2020-05-20 14:07:29 -0600

    en proceso del alert, con fallas

--- src/routes/links.js
+++ src/routes/links.js
@@ -10,14 +10,30 @@ const datos = {
     'comentario': '',
     'importancia': ''
 }
+var f = new Date();
+var fecha = f.getDate() + "/" + (f.getMonth() + 1) + "/" + f.getFullYear();
+
+function alertMessage(req){
+ 
+   return message;
+}
 
 routes.get('/reg-hora', (req, res) => {
     res.render('pages/bitacora-hora.hbs');
 });
 
 routes.post('/reg-dia', (req,res) => {
-        console.log(req.body);
-        res.send('mensaje');
+    message={
+        type:'danger',
+        intro:'empty',
+        message:req
+   }
+        datos.fecha= fecha;
+        if(req.body.actividad === 'Seleccione la Actividad'){
+        }
+       res.redirect('/links/reg-dia')
+       
+      
 r => console.log('error post reg-dia')});
 
 routes.get('/reg-dia', (req, res) => {
